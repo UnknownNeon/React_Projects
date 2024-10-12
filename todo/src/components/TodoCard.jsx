efd76e5ce2e45f1f104a4de2e99dad2809240327
Xen <twidlestick@gmail.com>
2024-10-12 05:38:38 +0530

    ...

--- todo/src/components/TodoCard.jsx
+++ todo/src/components/TodoCard.jsx
@@ -6,9 +6,9 @@ const TodoCard = (props) => {
   const todo = todos[todoIndex];
 
   return (
-    <div className='card color_blue'>
-      <h2 className='color_blue card-name'>Name : {todo.info} </h2>
-      <div className='color_blue'> 
+    <div className='card color_cust'>
+      <h2 className='color_cust card-name'>Name : {todo.info} </h2>
+      <div className='color_cust btn-card'> 
         <button>Done </button>
         <button>Delete </button>
       </div>
